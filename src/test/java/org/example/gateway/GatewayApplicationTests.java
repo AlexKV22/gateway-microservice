1d2c410d8b64305716681bfc155e4bd9e4554584
aleksandrkakovkin <aleksandrkakovkin44@gmail.com>
2025-07-26 18:21:13 +0300

    Add test method

--- src/test/java/org/example/gateway/GatewayApplicationTests.java
+++ src/test/java/org/example/gateway/GatewayApplicationTests.java
@@ -1,13 +1,31 @@
 package org.example.gateway;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GatewayApplicationTests {
 
+	private final TestRestTemplate restTemplate;
+
+	@Autowired
+	public GatewayApplicationTests(TestRestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws InterruptedException {
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8888/config-server/default", String.class);
+		Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+		Thread.sleep(10000);
+		ResponseEntity<String> responseEureka = restTemplate.getForEntity("http://localhost:8761/eureka/apps", String.class);
+		Assertions.assertEquals(HttpStatus.OK, responseEureka.getStatusCode());
+		Assertions.assertTrue(responseEureka.getBody().toLowerCase().contains("gateway"));
 	}
 
 }
