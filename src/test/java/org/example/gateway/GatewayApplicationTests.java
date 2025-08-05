e8b4641bb55c38f16216a492a005876ceb7eb7c5
aleksandrkakovkin <aleksandrkakovkin44@gmail.com>
2025-08-05 23:53:31 +0300

    Refactor test methods

--- src/test/java/org/example/gateway/GatewayApplicationTests.java
+++ src/test/java/org/example/gateway/GatewayApplicationTests.java
@@ -1,7 +1,6 @@
 package org.example.gateway;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Disabled
 class GatewayApplicationTests {
 
 	private final TestRestTemplate restTemplate;
@@ -21,13 +19,17 @@ class GatewayApplicationTests {
 	}
 
 	@Test
-	void contextLoads() throws InterruptedException {
-		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8888/config-server/default", String.class);
+	void contextLoadsConfigServerTest() {
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8888/gateway/default", String.class);
 		Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+		Assertions.assertTrue(response.getBody().toLowerCase().contains("gateway"));
+	}
+
+	@Test
+	void contextRegisterEurekaServerTest() throws InterruptedException {
 		Thread.sleep(14000);
 		ResponseEntity<String> responseEureka = restTemplate.getForEntity("http://localhost:8761/eureka/apps", String.class);
 		Assertions.assertEquals(HttpStatus.OK, responseEureka.getStatusCode());
 		Assertions.assertTrue(responseEureka.getBody().toLowerCase().contains("gateway"));
 	}
-
 }
